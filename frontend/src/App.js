482fb4c02823d1e9e1e6784dc49ebc7db6dc1c70
Du Pham <ngocdu171@gmail.com>
2021-10-12 19:46:53 +0300

    study react-quill 02

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactQuill from "react-quill";
 import Add from "./components/Add";
 import Home from "./components/Home";
 import News from "./components/News/News";
@@ -9,9 +10,47 @@ const App = () => {
     <div>
       <Home />
       <News />
-      <Add />
+      <ReactQuill
+      placeholder="write something..."
+      modules={App.modules}
+      formats={App.formats}
+      />
+      <p>hasgdhgasd</p>
     </div>
   );
 };
+
+App.modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { header: [3, 4, 5, 6] }, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["link", "image", "video"],
+    ["clean"],
+    ["code-block"],
+  ],
+};
+
+App.formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "align",
+  "strike",
+  "blockquote",
+  "background",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video",
+  "color",
+  "code-block",
+];
 
 export default App;
--- frontend/src/components/Add.js
+++ frontend/src/components/Add.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
-import EditorToolbar, { formats, modules } from "./EditorToolbar"
-import { Button, Container, Form, FormGroup, Input, Label, Row } from "reactstrap";
+import EditorToolbar, { formats, modules } from "./EditorToolbar";
+import {
+  Button,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from "reactstrap";
 import ReactQuill from "react-quill";
 
-export default function Add() {
-
-    const [isError, setError] = useState(null)
+function Add() {
+  const [isError, setError] = useState(null);
   return (
-    <Container>
+    <div>
+      <ReactQuill placeholder="write something..." 
+      formats={Add.formats}
+      modules={Add.modules}
+      />
       <Row>
         <Form>
           <h3>Add</h3>
@@ -18,31 +29,45 @@ export default function Add() {
             </FormGroup>
             <FormGroup>
               <label>Description</label>
-              <EditorToolbar toolbarId={'t1'}/>
-              <ReactQuill 
-              theme="snow"
-              placeholder="Write something here..."
-              modules={modules('t1')}
-              formats={formats}
-              />
-            </FormGroup>
-            <FormGroup>
-              <label>Additional Information</label>
-              <EditorToolbar toolbarId={'t2'}/>
-              <ReactQuill 
-              theme="snow"
-              placeholder="Write something here..."
-              modules={modules('t2')}
-              formats={formats}
-              />
-            </FormGroup>
-            {isError !== null && <div className="errors"> {isError} </div>}
-            <FormGroup>
-            <Button color="info"> Submit </Button>
             </FormGroup>
           </Row>
         </Form>
       </Row>
-    </Container>
+    </div>
   );
+}
+
+Add.modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { header: [3, 4, 5, 6] }, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["link", "image", "video"],
+    ["clean"],
+    ["code-block"],
+  ],
 };
+
+Add.formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "align",
+  "strike",
+  "blockquote",
+  "background",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video",
+  "color",
+  "code-block",
+];
+
+export default Add;
